dd3ce25d14f5337a7c611555d2fa2ddf6deb6d8d
Simon Boehm <simon@siboehm.com>
2021-04-23 08:20:04 +0200

    Rename simplekv imports to minimalkv

--- tests/storefact/test_gcstore_creation.py
+++ tests/storefact/test_gcstore_creation.py
@@ -3,8 +3,9 @@ import json
 import pathlib
 
 import pytest
-import storefact
-from storefact._store_creation import create_store
+
+from minimalkv import get_store_from_url, url2dict
+from minimalkv._store_creation import create_store
 
 storage = pytest.importorskip("google.cloud.storage")
 from google.auth.credentials import AnonymousCredentials
@@ -12,8 +13,8 @@ from google.auth.exceptions import RefreshError
 
 
 def test_create_store_gcstore(mocker):
-    mock_hgcstore = mocker.patch("storefact._hstores.HGoogleCloudStore")
-    mock_gcstore = mocker.patch("simplekv.net.gcstore.GoogleCloudStore")
+    mock_hgcstore = mocker.patch("minimalkv._hstores.HGoogleCloudStore")
+    mock_gcstore = mocker.patch("minimalkv.net.gcstore.GoogleCloudStore")
 
     anon_credentials = AnonymousCredentials()
     create_store(
@@ -35,7 +36,7 @@ def test_create_store_gcstore(mocker):
 
 
 def test_create_store_hgcstore(mocker):
-    mock_hgcstore = mocker.patch("storefact._hstores.HGoogleCloudStore")
+    mock_hgcstore = mocker.patch("minimalkv._hstores.HGoogleCloudStore")
 
     anon_credentials = AnonymousCredentials()
     create_store(
@@ -67,11 +68,13 @@ SHORT_URL = (
     },
 )
 ACTUAL_URL = (
-    f"gcs://{base64.urlsafe_b64encode(pathlib.Path('tests/gcstore_cred_example.json').read_bytes()).decode()}"
+    f"gcs://{base64.urlsafe_b64encode(pathlib.Path('tests/storefact/gcstore_cred_example.json').read_bytes()).decode()}"
     f"@default_bucket?create_if_missing=false",
     {
         "type": "gcs",
-        "credentials": pathlib.Path("tests/gcstore_cred_example.json").read_bytes(),
+        "credentials": pathlib.Path(
+            "tests/storefact/gcstore_cred_example.json"
+        ).read_bytes(),
         "bucket_name": "default_bucket",
         "create_if_missing": False,
     },
@@ -80,19 +83,19 @@ ACTUAL_URL = (
 
 @pytest.mark.parametrize("url, expected", [SHORT_URL, ACTUAL_URL])
 def test_url2dict(url, expected):
-    assert storefact.url2dict(url) == expected
+    assert url2dict(url) == expected
 
 
 def test_json_decode():
     url, _ = ACTUAL_URL
-    creds = storefact.url2dict(url)["credentials"]
-    with open("tests/gcstore_cred_example.json") as file:
+    creds = url2dict(url)["credentials"]
+    with open("tests/storefact/gcstore_cred_example.json") as file:
         assert json.loads(creds) == json.load(file)
 
 
 def test_complete():
     url, expected = ACTUAL_URL
-    store = storefact.get_store_from_url(url)
+    store = get_store_from_url(url)
     assert store.bucket_name == expected["bucket_name"]
     assert store._client.project == "central-splice-296415"
     with pytest.raises(RefreshError):
