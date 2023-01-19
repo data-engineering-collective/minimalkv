ebc838ac6ecca0c5b67b7432c91d22b0ed749a36
Simon Bohnen <mail@simonbohnen.me>
2023-01-19 17:20:11 +0100

    Remove stuff not related to s3

---
+++ tests/storefact/test_gcstore_creation.py
@@ -0,0 +1,105 @@
+import base64
+import json
+import pathlib
+
+import pytest
+
+from minimalkv._get_store import get_store_from_url
+from minimalkv._store_creation import create_store
+from minimalkv._urls import url2dict
+from minimalkv.net.gcstore import GoogleCloudStore
+
+storage = pytest.importorskip("google.cloud.storage")
+from google.auth.credentials import AnonymousCredentials
+from google.auth.exceptions import RefreshError
+
+
+def test_create_store_gcstore(mocker):
+    mock_hgcstore = mocker.patch("minimalkv._hstores.HGoogleCloudStore")
+    mock_gcstore = mocker.patch("minimalkv.net.gcstore.GoogleCloudStore")
+
+    anon_credentials = AnonymousCredentials()
+    create_store(
+        "gcs",
+        {
+            "credentials": anon_credentials,
+            "bucket_name": "test_bucket",
+            "create_if_missing": True,
+            "bucket_creation_location": "EUROPE-WEST1",
+        },
+    )
+    mock_gcstore.assert_called_once_with(
+        credentials=anon_credentials,
+        bucket_name="test_bucket",
+        create_if_missing=True,
+        bucket_creation_location="EUROPE-WEST1",
+    )
+    mock_hgcstore.assert_not_called()
+
+
+def test_create_store_hgcstore(mocker):
+    mock_hgcstore = mocker.patch("minimalkv._hstores.HGoogleCloudStore")
+
+    anon_credentials = AnonymousCredentials()
+    create_store(
+        "hgcs",
+        {
+            "credentials": anon_credentials,
+            "bucket_name": "test_bucket",
+            "create_if_missing": True,
+            "bucket_creation_location": "EUROPE-WEST1",
+        },
+    )
+    mock_hgcstore.assert_called_once_with(
+        credentials=anon_credentials,
+        bucket_name="test_bucket",
+        create_if_missing=True,
+        bucket_creation_location="EUROPE-WEST1",
+    )
+
+
+SHORT_URL = (
+    f"gcs://{base64.urlsafe_b64encode(b'some bytes+/=asdf').decode()}"
+    f"@bucket_name?create_if_missing=true&bucket_creation_location=WESTINDIES",
+    {
+        "type": "gcs",
+        "credentials": b"some bytes+/=asdf",
+        "bucket_name": "bucket_name",
+        "create_if_missing": True,
+        "bucket_creation_location": "WESTINDIES",
+    },
+)
+ACTUAL_URL = (
+    f"gcs://{base64.urlsafe_b64encode(pathlib.Path('tests/storefact/gcstore_cred_example.json').read_bytes()).decode()}"
+    f"@default_bucket?create_if_missing=false",
+    {
+        "type": "gcs",
+        "credentials": pathlib.Path(
+            "tests/storefact/gcstore_cred_example.json"
+        ).read_bytes(),
+        "bucket_name": "default_bucket",
+        "create_if_missing": False,
+    },
+)
+
+
+@pytest.mark.parametrize("url, expected", [SHORT_URL, ACTUAL_URL])
+def test_url2dict(url, expected):
+    assert url2dict(url) == expected
+
+
+def test_json_decode():
+    url, _ = ACTUAL_URL
+    creds = url2dict(url)["credentials"]
+    with open("tests/storefact/gcstore_cred_example.json") as file:
+        assert json.loads(creds) == json.load(file)
+
+
+def test_complete():
+    url, expected = ACTUAL_URL
+    store = get_store_from_url(url)
+    assert isinstance(store, GoogleCloudStore)
+    assert store.bucket_name == expected["bucket_name"]  # type: ignore
+    assert store.project_name == "central-splice-296415"  # type: ignore
+    with pytest.raises(RefreshError):
+        store.get("somekey")
---
+++ tests/storefact/test_urls.py
@@ -0,0 +1,95 @@
+import pytest
+
+from minimalkv._get_store import get_store_from_url
+from minimalkv._urls import url2dict
+from minimalkv.decorator import ReadOnlyDecorator
+
+good_urls = [
+    (
+        "azure://MYACCOUNT:dead%2Fbeef@1buc-ket1?param1=foo&param2=🍺&eat_more_🍎=1&create_if_missing=true",
+        dict(
+            type="azure",
+            account_name="MYACCOUNT",
+            account_key="dead/beef",
+            container="1buc-ket1",
+            create_if_missing=True,
+        ),
+    ),
+    (
+        "azure://MYACCOUNT:deadbeef@1bucket1?param1=foo&param2=🍺&eat_more_🍎=1&create_if_missing=true",
+        dict(
+            type="azure",
+            account_name="MYACCOUNT",
+            account_key="deadbeef",
+            container="1bucket1",
+            create_if_missing=True,
+        ),
+    ),
+    (
+        "azure://MYACCOUNT:deadbeef@1bucket1?param1=foo&param2=🍺&eat_more_🍎=&max_connections=5",
+        dict(
+            type="azure",
+            account_name="MYACCOUNT",
+            account_key="deadbeef",
+            container="1bucket1",
+            max_connections=5,
+        ),
+    ),
+    (
+        "azure://MYACCOUNT:deadbeef@1bucket1?param1=foo&param2=🍺&eat_more_🍎=&max_connections=5&max_block_size=4194304&&max_single_put_size=67108864",
+        dict(
+            type="azure",
+            account_name="MYACCOUNT",
+            account_key="deadbeef",
+            container="1bucket1",
+            max_connections=5,
+            max_block_size=["4194304"],
+            max_single_put_size=["67108864"],
+        ),
+    ),
+    ("fs://this/is/a/relative/path", dict(type="fs", path="this/is/a/relative/path")),
+    ("fs:///an/absolute/path", dict(type="fs", path="/an/absolute/path")),
+    (
+        "s3://access_key:secret_key@endpoint:1234/bucketname",
+        dict(
+            type="s3",
+            host="endpoint:1234",
+            access_key="access_key",
+            secret_key="secret_key",
+            bucket="bucketname",
+        ),
+    ),
+    ("redis:///2", dict(type="redis", host="localhost", db=2)),
+    ("memory://#wrap:readonly", {"type": "memory", "wrap": "readonly"}),
+    ("memory://", dict(type="memory")),
+]
+
+bad_urls = [
+    (
+        "azure://MYACCOUNT:deadb/eef@1buc-ket1?param1=foo&param2=🍺&eat_more_🍎=1&create_if_missing=true",
+        ValueError,
+    ),
+]
+
+
+def test_raise_on_invalid_store():
+    with pytest.raises(ValueError):
+        url2dict("dummy://foo/bar")
+
+
+@pytest.mark.parametrize("url, expected", good_urls)
+def test_url2dict(url, expected):
+    assert url2dict(url) == expected
+
+
+@pytest.mark.parametrize("url, raises", bad_urls)
+def test_bad_url2dict(url, raises):
+    with pytest.raises(raises):
+        url2dict(url)
+
+
+def test_roundtrip():
+    assert isinstance(
+        get_store_from_url("memory://#wrap:readonly"),
+        ReadOnlyDecorator,
+    )
